eaaef1969ec65cc6866307447607d38273a782e1
berkayguzel06 <berkayguzel43@gmail.com>
2023-11-28 20:30:46 +0300

    added connection react and server
    there is a bug to fix that while using cons

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, {useEffect } from 'react';
+
 
 function App() {
+  useEffect(() => {
+    axios.get('http://127.0.0.1:5000/Posts').then((response)=>{
+      console.log(response.data);
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App"></div>
   );
 }
-
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const db = require('./models') // gets db model from models folder
+const cors = require('cors'); // allows cross origin resource sharing
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 const postRouter = require('./Routers/Posts'); // gets posts router from router folder
 app.use("/posts", postRouter); // uses posts router
-
+app.use(cors({
+  origin: 'http://localhost:3000', // allows cross origin resource sharing from localhost:3000
+  credentials: true
+}));
 
 db.sequelize.sync().then(() => { // when api is ready check model folder and sync the database every table
   app.listen(PORT, () => {        // starts the server

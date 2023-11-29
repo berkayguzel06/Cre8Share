77e10daadb4dbc02fac3742a3c3758fb825e117d
berkayguzel06 <berkayguzel43@gmail.com>
2023-11-29 19:55:35 +0300

    commend lines

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import axios from 'axios';
-import React, {useEffect } from 'react';
-
+import React, { useEffect } from 'react';
 
 function App() {
+  // useEffect hook is used to perform side effects in function components
   useEffect(() => {
-    axios.get('http://127.0.0.1:5000/Posts').then((response)=>{
+    // Axios is used to make an HTTP GET request to 'http://127.0.0.1:5000/Posts'
+    axios.get('http://127.0.0.1:5000/Posts').then((response) => {
+      // Log the data received from the server to the console
       console.log(response.data);
     });
-  }, []);
+  }, []); // The empty dependency array ensures that this effect runs once after the initial render
+
+  // Render an empty div with the 'App' class
   return (
     <div className="App"></div>
   );
--- server/server.js
+++ server/server.js
@@ -1,23 +1,25 @@
 const express = require('express');
-const db = require('./models') // gets db model from models folder
-const cors = require('cors'); // allows cross origin resource sharing
+const db = require('./models'); // Importing the database models
+const cors = require('cors'); // Importing the CORS middleware
+const app = express(); // Creating an instance of the Express application
+const PORT = process.env.PORT || 5000; // Setting the port for the server
+const postRouter = require('./Routers/Posts'); // Importing the router for handling posts
 
-const app = express();
-const PORT = process.env.PORT || 5000;
-const postRouter = require('./Routers/Posts'); // gets posts router from router folder
 // Enable CORS with a custom configuration
 app.use(cors({
-  origin: 'http://localhost:3000',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  optionsSuccessStatus: 204,
+  origin: 'http://localhost:3000', // Allowing requests from 'http://localhost:3000'
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowing specified HTTP methods
+  credentials: true, // Allowing credentials (e.g., cookies, authorization headers)
+  optionsSuccessStatus: 204, // Setting the HTTP status code for preflight requests
 }));
 
-app.use("/posts", postRouter);
+app.use("/posts", postRouter); // Using the postRouter for requests starting with '/posts'
 
-db.sequelize.sync().then(() => { // when api is ready check model folder and sync the database every table
-  app.listen(PORT, () => {        // starts the server
+db.sequelize.sync().then(() => {
+  // Synchronizing the database models and starting the server
+  app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
 });
 
+// End of the server setup

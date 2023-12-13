c02600e95d70b9726b33822ae435aecbdd799c96
ygtonlu <ygtonlu@outlook.com>
2023-12-13 20:48:31 +0300

    routers update
    .

--- server/Routers/CommentReports.js
+++ server/Routers/CommentReports.js
@@ -1,11 +1,11 @@
 const express = require('express'); // Importing the 'express' library to create a router 
 const router = express.Router(); // Creating an instance of an Express router
-const { Comment } = require('../models'); // Importing the 'Comment' model from the '../models' directory
+const { CommentReport } = require('../models'); // Importing the 'comment report' model from the '../models' directory
 
 // Handling HTTP GET requests to the root path ("/")
 router.get("/", async (req, res) => {
-    // Using Sequelize's 'findAll' method to retrieve all comments from the database
-    const listofComment = await Comment.findAll();
+    // Using Sequelize's 'findAll' method to retrieve all comment reports from the database
+    const listofCommentReport = await CommentReport.findAll();
     // Sending the list of users as a JSON response
     res.json(listofComments);
 });
@@ -13,8 +13,8 @@ router.get("/", async (req, res) => {
 // Handling HTTP POST requests to the root path ("/")
 router.post("/", async (req, res) => {
     const post = req.body; // Extracting the request body, which should contain data for creating a new user
-    await Comment.create(post); // Using Sequelize's 'create' method to add a new comment to the database
-    res.send(post); // Sending the comment data as a response
+    await CommentReport.create(post); // Using Sequelize's 'create' method to add a new comment report to the database
+    res.send(post); // Sending the comment report data as a response
 });
 
 module.exports = router;

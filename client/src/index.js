2485369c920454a48366d75ac3b5398dc182bdca
HonoreDHonneur <onurrk24@gmail.com>
2023-12-16 11:58:24 +0300

    Login Screen

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+//import ReactDOM1 from 'react-dom';
+import AppLogin from './Login';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+//ReactDOM1.createRoot(<App1/>,document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
+    <AppLogin/>
   </React.StrictMode>
 );
 

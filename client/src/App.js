680d44e19caf1651bd2ba762a1a8f80b470b1620
berkayguzel06 <98205992+berkayguzel06@users.noreply.github.com>
2023-12-16 19:49:52 +0300

    css files added

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./css/App.css";
 import { BrowserRouter as Router, Route, Switch, Link, Routes } from "react-router-dom";
 import Login from "./pages/Login.js";
 import Home from "./pages/Home.js";
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 //import ReactDOM1 from 'react-dom';
-import './index.css';
+import './css/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 

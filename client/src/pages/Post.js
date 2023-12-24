e05bd8ffa2a7d612b2f8aa0d00d48c873301c463
ygtonlu <ygtonlu@outlook.com>
2023-12-24 18:34:01 +0300

    make a comment added
    1- username'i simdilik elimle attim 1 olarak
    2 - postId'leri null olarak atiyor database'den elimle 1 girdim idleri commentlerin oyle gordum.
    3 - css'i yapilmadigi icin orantisiz duruyor ama sorun yok.
    bu debuglari yapicaz sirada bunlar var.

--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -1,4 +1,3 @@
-// PostDetail.js
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
@@ -6,6 +5,9 @@ import axios from 'axios';
 const Post = () => {
   const { postId } = useParams();
   const [post, setPost] = useState(null);
+  const [comments, setComments] = useState(null);
+  const [newComment, setNewComment] = useState('');
+  
   const arrayBufferToBase64 = (buffer) => {
     const binary = new Uint8Array(buffer.data).reduce(
       (binaryString, byte) => binaryString + String.fromCharCode(byte),
@@ -14,24 +16,66 @@ const Post = () => {
     return window.btoa(binary);
   };
 
-  useEffect(() => {
-    const fetchPost = async () => {
-      try {
-        const response = await axios.get(`http://localhost:5000/post/${postId}`);
-        console.log(response.data);
-  
-        if (response.data && response.data.content) {
-          setPost(response.data);
-        } else {
-          console.error('Invalid data format or missing content:', response.data);
-        }
-      } catch (error) {
-        console.error('Error fetching post details:', error);
+  const fetchPost = async () => {
+    try {
+      const response = await axios.get(`http://localhost:5000/post/${postId}`);
+      console.log(response.data);
+
+      if (response.data && response.data.content) {
+        setPost(response.data);
+      } else {
+        console.error('Invalid data format or missing content:', response.data);
       }
-    };
-  
+    } catch (error) {
+      console.error('Error fetching post details:', error);
+    }
+  };
+
+  const fetchComments = async () => {
+    try {
+      const response = await axios.get(`http://localhost:5000/comment/${postId}`);
+      console.log(response.data);
+
+      if (Array.isArray(response.data)) {
+        setComments(response.data);
+      } else {
+        console.error('Invalid data format:', response.data);
+      }
+    } catch (error) {
+      console.error('Error fetching comments:', error);
+    }
+  };
+
+  useEffect(() => {
     fetchPost();
+    fetchComments();
   }, [postId]);
+
+  const handleCommentChange = (e) => {
+    setNewComment(e.target.value);
+  };
+
+  const handleCommentSubmit = async () => {
+    try {
+        // Assuming you have the user ID from somewhere, replace 'USER_ID' with the actual user ID
+
+        console.log('postId:', postId)
+        console.log('newComment:', newComment)
+        axios.post('http://localhost:5000/comment/create', {
+            postID: postId,
+            userID: 1,
+            content: newComment,
+        });
+
+        // Refetch comments after submitting a new comment
+        fetchComments();
+
+        // Clear the input field after submitting
+        setNewComment('');
+    } catch (error) {
+        console.error('Error submitting comment:', error);
+    }
+};
 
   if (!post) {
     return <p>Loading...</p>;
@@ -40,13 +84,41 @@ const Post = () => {
   return (
     <div>
       <h2>Post Details</h2>
-      <p>title: {post.title}</p>
+      <p>Title: {post.title}</p>
       <img
-          src={`data:image/png;base64,${arrayBufferToBase64(post.content)}`}
-          alt={`Post ID: ${post.id}`}
+        src={`data:image/png;base64,${arrayBufferToBase64(post.content)}`}
+        alt={`Post ID: ${post.id}`}
+      />
+
+      {/* Comment input and submit button */}
+      <div>
+        <label htmlFor="comment">Make a Comment:</label>
+        <input
+          type="text"
+          id="comment"
+          value={newComment}
+          onChange={handleCommentChange}
         />
-      
-      {/* Add more details as needed */}
+        <button onClick={handleCommentSubmit}>Submit</button>
+      </div>
+
+      {/* Display comments */}
+      <div>
+        <h3>Comments</h3>
+        {comments.length >= 0 ? (
+          <ul>
+            {comments.map(comment => (
+              <li key={comment.id}>
+                {/* Display comment details as needed */}
+                <p>User: {comment.username}</p>
+                <p>{comment.content}</p>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No comments available.</p>
+        )}
+      </div>
     </div>
   );
 };
--- server/Routers/Comments.js
+++ server/Routers/Comments.js
@@ -17,6 +17,24 @@ router.get("/commentcount", async (req, res) => {
     }
 });
 
+router.get("/:postID", async (req, res) => {
+    const postID = req.params.postID; // Extracting the post ID from the URL parameters
+    try {
+        const listOfComments = await Comment.findAll({ where: { PostID: postID } });
+      if (listOfComments) {
+        res.json(listOfComments);
+      } else {
+        console.log("Comments not found with ID:", postID);
+        // If the post with the given ID is not found, send a 404 status
+        res.status(404).json({ error: 'Comments are not found' });
+      }
+    } catch (error) {
+      console.error('Error fetching comments :', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
+
 // Handling HTTP POST requests to the "/delete" path
 router.delete("/:commentID", async (req, res) => {
     const { commentID } = req.body;
@@ -26,14 +44,13 @@ router.delete("/:commentID", async (req, res) => {
 
 // Handling HTTP POST requests to the "/create" path
 router.post("/create", async (req, res) => {
+    const { postID, userID, content } = req.body;
     try {
-        const { postID, userID, content } = req.body;
-        const newComment = await Comment.create({ PostID: postID, userid: userID, content:content });
-        res.json(newComment);
+        const newComment = await Comment.create({ PostID: postID, userid: userID, content: content });
+        res.status(201).json({ newComment });
     } catch (error) {
-        console.error(error);
+        console.error('Error creating comment:', error);
         res.status(500).send('Internal Server Error');
     }
 });
-
 module.exports = router;

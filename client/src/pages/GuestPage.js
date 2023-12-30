a3a5c29ba787fc718d0f990e4eee3b5da9d9cc1b
SELİM ARSLAN <akif.selim.aa@gmail.com>
2023-12-30 21:29:03 +0300

    guestpagedeneme

--- client/src/pages/GuestPage.js
+++ client/src/pages/GuestPage.js
@@ -1,29 +1,46 @@
 import React, { useState, useEffect } from 'react';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import Masonry from 'react-masonry-css';
 import '../css/GuestPage.css';
 
 const GuestPage = () => {
   const [listOfPosts, setListOfPosts] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost:5000/post/guestpage')  // Assuming you're using GET to retrieve posts
-      .then(response => {
-        console.log('Response:', response.data);
-        console.log('Token:', sessionStorage.getItem("accessToken"));
-        // Ensure that the response.data is an array before calling setListOfPosts
+    axios
+      .get('http://localhost:5000/post/guestpage')
+      .then((response) => {
         if (Array.isArray(response.data)) {
           setListOfPosts(response.data);
         } else {
           console.error('Invalid data format:', response.data);
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.error('Error fetching posts:', error);
       });
   }, []);
 
-  // Function to convert binary data to base64
+  const getTimeAgo = (createdAt) => {
+    // Your existing code for formatting time difference
+  };
+
+  const navigate = useNavigate();
+
+  const navigateLogin = () => {
+    navigate('/login');
+  };
+
+  const navigateRegister = () => {
+    navigate('/register');
+  };
+
+  const breakpoints = {
+    3000: 4,
+    1100: 2,
+  };
+
   const arrayBufferToBase64 = (buffer) => {
     const binary = new Uint8Array(buffer.data).reduce(
       (binaryString, byte) => binaryString + String.fromCharCode(byte),
@@ -32,61 +49,33 @@ const GuestPage = () => {
     return window.btoa(binary);
   };
 
-  // Function to format the time difference
-  const getTimeAgo = (createdAt) => {
-    const now = new Date();
-    const createdDate = new Date(createdAt);
-    const timeDifference = now - createdDate;
-    const hoursAgo = Math.floor(timeDifference / (1000 * 60 * 60));
-    return `${hoursAgo} hours ago`;
-  };
-
-  const navigate = useNavigate();
-
-  const navigateLogin = () => {
-    // navigate to /login
-    navigate('/login');
-  };
-
-  const navigateRegister = () => {
-    // navigate to /register
-    navigate('/register');
-  };
-  
   return (
     <div>
-      <meta charSet="UTF-8" />
-      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>GuestPage</title>
-      <link rel="stylesheet" href="style.css" />
       <div className="wrapper">
         <form action="">
-          {/* Display the latest posts */}
           <div>
             <h2>Latest Posts</h2>
             {listOfPosts.length > 0 ? (
-    <ul>
-        {listOfPosts.map(post => (
-            <li key={post.id}>
-                {/* <p>Posted by {post.User.username}</p> */}
-                {/* <p>Posted {getTimeAgo(post.createdAt)} ago</p> */}
-                {/* Display user's name instead of user ID */}
-                
-                
-                {/* Convert the binary data to base64 and set as src */}
-                <img
-                    src={`data:image/png;base64,${arrayBufferToBase64(post.content)}`}
-                    alt={`Post: ${post.id}`}
-                />
-            </li>
-        ))}
-    </ul>
-    ) : (
-        <p>No posts available.</p>
-    )}
+              <Masonry
+                breakpointCols={breakpoints}
+                className="my-masonry-grid"
+                columnClassName="my-masonry-grid_column"
+              >
+                {listOfPosts.map((post) => (
+                  <div key={post.id} className="post-item">
+                    {/* Display post content here */}
+                    <img
+                      src={`data:image/png;base64,${arrayBufferToBase64(post.content)}`}
+                      alt={`Post: ${post.id}`}
+                    />
+                    {/* Other post details */}
+                  </div>
+                ))}
+              </Masonry>
+            ) : (
+              <p>No posts available.</p>
+            )}
           </div>
-          {/* Add sign-in and sign-up buttons */}
           <div>
             <button onClick={navigateLogin}>Sign In</button>
             <button onClick={navigateRegister}>Sign Up</button>

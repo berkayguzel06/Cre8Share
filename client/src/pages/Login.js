fb5c02e331eaab4fac1475aec25d337f2bfadb6e
SELİM ARSLAN <akif.selim.aa@gmail.com>
2023-12-31 12:06:28 +0300

    login ekran düzeldi

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -92,7 +92,7 @@ const AppLogin = () => {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>Log In</title>
       <link rel="stylesheet" href="style.css" />
-      <div className="wrapper" style={{ zIndex: '10' }}>
+      <div className="wrapper" style={{ zIndex: 10 }}>
         <form>
           <h1>Login</h1>
           <div className="input-box">

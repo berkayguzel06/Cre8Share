af7fa5e4636ca8bfa7c888eff8c642e381e835f3
SELİM ARSLAN <akif.selim.aa@gmail.com>
2024-01-01 22:38:17 +0300

    admine header eklendi, view all users düzeldi

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import "../css/Admin.css";
+import Header from './Header';
 
 const Admin = () => {
   const [listOfPosts, setListOfPosts] = useState([]);
@@ -51,7 +52,7 @@ const Admin = () => {
   };
 
   const handleLogin = () => {
-    axios.post('http://localhost:5000/admin/login', {username: username, password: password}).then((response) => {
+    axios.post('http://localhost:5000/admin/login', { username: username, password: password }).then((response) => {
       if (response.data.token) {
         localStorage.setItem('adminToken', response.data.token);
         setAdminAuthentication(true);
@@ -71,17 +72,17 @@ const Admin = () => {
     });
   };
   const deleteUser = (userId) => {
-      axios.delete(`http://localhost:5000/user/${userId}`).then((response) => {
-        console.log('User deleted:', response.data);
-      }).catch((error) => {
-        console.error('Error deleting post:', error);
-      });
+    axios.delete(`http://localhost:5000/user/${userId}`).then((response) => {
+      console.log('User deleted:', response.data);
+    }).catch((error) => {
+      console.error('Error deleting post:', error);
+    });
 
-      axios.delete(`http://localhost:5000/friend/${userId}`).then((response) => {
-        console.log('Friend deleted:', response.data);
-      }).catch((error) => {
-        console.error('Error deleting post:', error);
-      });
+    axios.delete(`http://localhost:5000/friend/${userId}`).then((response) => {
+      console.log('Friend deleted:', response.data);
+    }).catch((error) => {
+      console.error('Error deleting post:', error);
+    });
 
   };
   const deleteComment = (commentId) => {
@@ -94,75 +95,78 @@ const Admin = () => {
 
   return (
     <div>
-      {isAdminAuthenticated ? (
-        <>
-          <h1>Admin Page</h1>
+      <Header />
+      <div>
+        {isAdminAuthenticated ? (
+          <>
+            <h1>Admin Page</h1>
+            <div>
+              <button onClick={() => setSelectedAction('posts')}>Get Posts</button>
+              <button onClick={() => setSelectedAction('users')}>Get Users</button>
+              <button onClick={() => setSelectedAction('comments')}>Get Comments</button>
+            </div>
+            <div className="data-container">
+              {selectedAction === 'posts' && (
+                <div className="posts-container">
+                  {listOfPosts.map((post) => (
+                    <div key={post.id} className="post-item">
+                      {/* Display post info */}
+                      <p>Post ID: {post.id}</p>
+                      <p>Tag: {post.title}</p>
+                      <p>User Id: {post.User.id}</p>
+                      <p>Like: {post.like}</p>
+                      <p>Report: {post.report}</p>
+                      <img src={`data:image/png;base64,${arrayBufferToBase64(post.content)}`} alt="post" />
+                      <button onClick={() => deletePost(post.id)}>Delete Post</button>
+                    </div>
+                  ))}
+                </div>
+              )}
+              {selectedAction === 'users' && (
+                <div className="users-container">
+                  {listOfUsers.map((user) => (
+                    <div key={user.id} className="user-item">
+                      {/* Display user info */}
+                      <p>User ID: {user.id}</p>
+                      <p>Username: {user.username}</p>
+                      <p>E-mail: {user.email}</p>
+                      <button onClick={() => deleteUser(user.id)}>Delete User</button>
+                    </div>
+                  ))}
+                </div>
+              )}
+              {selectedAction === 'comments' && (
+                <div className="comments-container">
+                  {listOfComments.map((comment) => (
+                    <div key={comment.id} className="comment-item">
+                      {/* Display comment info */}
+                      <p>Comment ID: {comment.id}</p>
+                      <p>User Id: {comment.UserId}</p>
+                      <p>Post Id: {comment.PostId}</p>
+                      <p>Post Id: {comment.PostId}</p>
+                      <p>Content: {comment.content}</p>
+                      <button onClick={() => deleteComment(comment.id)}>Delete Comment</button>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          </>
+        ) : (
           <div>
-            <button onClick={() => setSelectedAction('posts')}>Get Posts</button>
-            <button onClick={() => setSelectedAction('users')}>Get Users</button>
-            <button onClick={() => setSelectedAction('comments')}>Get Comments</button>
+            <h1>Admin Login</h1>
+            <label>
+              Username:
+              <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+            </label>
+            <label>
+              Password:
+              <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </label>
+            <button onClick={handleLogin}>Login</button>
           </div>
-          <div className="data-container">
-            {selectedAction === 'posts' && (
-              <div className="posts-container">
-                {listOfPosts.map((post) => (
-                  <div key={post.id} className="post-item">
-                    {/* Display post info */}
-                    <p>Post ID: {post.id}</p>
-                    <p>Tag: {post.title}</p>
-                    <p>User Id: {post.User.id}</p>
-                    <p>Like: {post.like}</p>
-                    <p>Report: {post.report}</p>
-                    <img src={`data:image/png;base64,${arrayBufferToBase64(post.content)}`} alt="post" />
-                    <button onClick={() => deletePost(post.id)}>Delete Post</button>
-                  </div>
-                ))}
-              </div>
-            )}
-            {selectedAction === 'users' && (
-              <div className="users-container">
-                {listOfUsers.map((user) => (
-                  <div key={user.id} className="user-item">
-                    {/* Display user info */}
-                    <p>User ID: {user.id}</p>
-                    <p>Username: {user.username}</p>
-                    <p>E-mail: {user.email}</p>
-                    <button onClick={() => deleteUser(user.id)}>Delete User</button>
-                  </div>
-                ))}
-              </div>
-            )}
-            {selectedAction === 'comments' && (
-              <div className="comments-container">
-                {listOfComments.map((comment) => (
-                  <div key={comment.id} className="comment-item">
-                    {/* Display comment info */}
-                    <p>Comment ID: {comment.id}</p>
-                    <p>User Id: {comment.UserId}</p>
-                    <p>Post Id: {comment.PostId}</p>
-                    <p>Post Id: {comment.PostId}</p>
-                    <p>Content: {comment.content}</p>
-                    <button onClick={() => deleteComment(comment.id)}>Delete Comment</button>
-                  </div>
-                ))}
-              </div>
-            )}
-          </div>
-        </>
-      ) : (
-        <div>
-          <h1>Admin Login</h1>
-          <label>
-            Username:
-            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-          </label>
-          <label>
-            Password:
-            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-          </label>
-          <button onClick={handleLogin}>Login</button>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
--- client/src/pages/ListedProfiles.js
+++ client/src/pages/ListedProfiles.js
@@ -1,19 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import '../css/ListedProfiles.css';
+import Header from './Header';
+import profileImage from '../images/pp.png';
 
 const ListedProfiles = () => {
   const location = useLocation();
   const { userSearchResults } = location.state || {};
+
+  const arrayBufferToBase64 = (buffer) => {
+    const binary = new Uint8Array(buffer.data).reduce(
+      (binaryString, byte) => binaryString + String.fromCharCode(byte),
+      ''
+    );
+    return window.btoa(binary);
+  };
+
   return (
     <div>
-      <h2>Listed Users</h2>
-      <ul>
-        {userSearchResults && userSearchResults.map((user) => (
-          <li key={user.id}>
-            <Link to={`/user/${user.username}`}>{user.username}</Link>
-          </li>
-        ))}
-      </ul>
+      <Header />
+      <div className="listed-profiless-container">
+        <h2>Listed Users</h2>
+        <div className="profiless-frame">
+          <ul className="profiless-list">
+            {userSearchResults &&
+              userSearchResults.map((user) => (
+                <li key={user.id} className="profiless-item">
+                  <Link to={`/user/${user.username}`}>
+                    {user.pfp ? (
+                      <img
+                        src={`data:image/png;base64,${arrayBufferToBase64(user.pfp)}`}
+                        className="profiless-image"
+                      />
+                    ) : (
+                      <img
+                        src={profileImage}
+                        className="profiless-image"
+                        alt="Default Profile Picture"
+                      />
+                    )}
+                    <span className="profiless-username">{user.username}</span>
+                  </Link>
+                </li>
+              ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };

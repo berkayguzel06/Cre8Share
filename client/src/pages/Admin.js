f51050eb6ca8d4440d7147fbae849823f8f3a333
berkayguzel06 <berkayguzel43@gmail.com>
2024-01-01 13:19:59 +0300

    admin
    admin functionalitites added

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,31 +1,44 @@
 // Admin.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
 import "../css/Admin.css";
 
 const Admin = () => {
   const [listOfPosts, setListOfPosts] = useState([]);
+  const [listOfUsers, setListOfUsers] = useState([]);
+  const [listOfComments, setListOfComments] = useState([]);
   const [isAdminAuthenticated, setAdminAuthentication] = useState(false);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [selectedAction, setSelectedAction] = useState(null);
 
   useEffect(() => {
-    if (isAdminAuthenticated) {
-      fetchPosts();
+    if (isAdminAuthenticated && selectedAction) {
+      fetchData();
     }
-  }, [isAdminAuthenticated]);
+  }, [isAdminAuthenticated, selectedAction]);
 
-  const fetchPosts = async () => {
+  const fetchData = async () => {
     try {
-      const response = await axios.get('http://localhost:5000/admin/post');
-      if (Array.isArray(response.data)) {
-        setListOfPosts(response.data);
-      } else {
-        console.error('Invalid data format:', response.data);
+      let response;
+      switch (selectedAction) {
+        case 'posts':
+          response = await axios.get('http://localhost:5000/admin/post');
+          setListOfPosts(response.data);
+          break;
+        case 'users':
+          response = await axios.get('http://localhost:5000/admin/user');
+          setListOfUsers(response.data);
+          break;
+        case 'comments':
+          response = await axios.get('http://localhost:5000/admin/comment');
+          setListOfComments(response.data);
+          break;
+        default:
+          console.error('Invalid action:', selectedAction);
       }
     } catch (error) {
-      console.error('Error fetching posts:', error);
+      console.error(`Error fetching ${selectedAction}:`, error);
     }
   };
 
@@ -50,28 +63,90 @@ const Admin = () => {
     });
   };
 
+  const deletePost = (postId) => {
+    axios.delete(`http://localhost:5000/admin/post/${postId}`).then((response) => {
+      console.log('Post deleted:', response.data);
+    }).catch((error) => {
+      console.error('Error deleting post:', error);
+    });
+  };
+  const deleteUser = (userId) => {
+      axios.delete(`http://localhost:5000/user/${userId}`).then((response) => {
+        console.log('User deleted:', response.data);
+      }).catch((error) => {
+        console.error('Error deleting post:', error);
+      });
+
+      axios.delete(`http://localhost:5000/friend/${userId}`).then((response) => {
+        console.log('Friend deleted:', response.data);
+      }).catch((error) => {
+        console.error('Error deleting post:', error);
+      });
+
+  };
+  const deleteComment = (commentId) => {
+    axios.delete(`http://localhost:5000/admin/comment/${commentId}`).then((response) => {
+      console.log('Comment deleted:', response.data);
+    }).catch((error) => {
+      console.error('Error deleting post:', error);
+    });
+  };
+
   return (
     <div>
       {isAdminAuthenticated ? (
         <>
           <h1>Admin Page</h1>
-          <div className="posts-container">
-            {listOfPosts.map((post) => (
-              <div key={post.id} className="post-container">
-                <Link to={`/user/${post.User.username}`}>
-                  <div className="username-overlay">{post.User.username}</div>
-                </Link>
-                <div className="likenumber">
-                  {post.like}
-                </div>
-                <Link to={`/post/${post.id}`}>
-                  <img
-                    src={`data:image/png;base64,${arrayBufferToBase64(post.content)}`}
-                    alt={`Post ID: ${post.id}`}
-                  />
-                </Link>
+          <div>
+            <button onClick={() => setSelectedAction('posts')}>Get Posts</button>
+            <button onClick={() => setSelectedAction('users')}>Get Users</button>
+            <button onClick={() => setSelectedAction('comments')}>Get Comments</button>
+          </div>
+          <div className="data-container">
+            {selectedAction === 'posts' && (
+              <div className="posts-container">
+                {listOfPosts.map((post) => (
+                  <div key={post.id} className="post-item">
+                    {/* Display post info */}
+                    <p>Post ID: {post.id}</p>
+                    <p>Tag: {post.title}</p>
+                    <p>User Id: {post.User.id}</p>
+                    <p>Like: {post.like}</p>
+                    <p>Report: {post.report}</p>
+                    <img src={`data:image/png;base64,${arrayBufferToBase64(post.content)}`} alt="post" />
+                    <button onClick={() => deletePost(post.id)}>Delete Post</button>
+                  </div>
+                ))}
               </div>
-            ))}
+            )}
+            {selectedAction === 'users' && (
+              <div className="users-container">
+                {listOfUsers.map((user) => (
+                  <div key={user.id} className="user-item">
+                    {/* Display user info */}
+                    <p>User ID: {user.id}</p>
+                    <p>Username: {user.username}</p>
+                    <p>E-mail: {user.email}</p>
+                    <button onClick={() => deleteUser(user.id)}>Delete User</button>
+                  </div>
+                ))}
+              </div>
+            )}
+            {selectedAction === 'comments' && (
+              <div className="comments-container">
+                {listOfComments.map((comment) => (
+                  <div key={comment.id} className="comment-item">
+                    {/* Display comment info */}
+                    <p>Comment ID: {comment.id}</p>
+                    <p>User Id: {comment.UserId}</p>
+                    <p>Post Id: {comment.PostId}</p>
+                    <p>Post Id: {comment.PostId}</p>
+                    <p>Content: {comment.content}</p>
+                    <button onClick={() => deleteComment(comment.id)}>Delete Comment</button>
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
         </>
       ) : (
--- server/Routers/Admin.js
+++ server/Routers/Admin.js
@@ -1,6 +1,6 @@
 const express = require('express'); // Importing the 'express' library to create a router 
 const router = express.Router(); // Creating an instance of an Express router
-const { Admin, Post, User } = require('../models'); // Importing the 'User' model from the '../models' directory
+const { Admin, Post, User, Comment } = require('../models'); // Importing the 'User' model from the '../models' directory
 const {sign} = require("jsonwebtoken")
 
 router.post("/login", async (req, res) => {
@@ -24,20 +24,81 @@ router.post("/login", async (req, res) => {
 
 // (validation) validateToken is a middleware that checks if the user is logged in then return posts
 router.get("/post", async (req, res) => {
-    try {
-        // Using Sequelize's 'findAll' method to retrieve all posts from the database
-        const listOfPosts = await Post.findAll({
-            include: {
-                model: User,
-                attributes: ['username','id'], // Include only the 'username' attribute of the User model
-            },
-        });
-        // Sending the list of posts as a JSON response
-        res.json(listOfPosts);
-    } catch (error) {
-        console.error('Error fetching posts:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
+  try {
+      // Using Sequelize's 'findAll' method to retrieve all posts from the database
+      const listOfPosts = await Post.findAll({
+          include: {
+              model: User,
+              attributes: ['username','id'], // Include only the 'username' attribute of the User model
+          },
+      });
+      // Sending the list of posts as a JSON response
+      res.json(listOfPosts);
+  } catch (error) {
+      console.error('Error fetching posts:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// (validation) validateToken is a middleware that checks if the user is logged in then return posts
+router.get("/user", async (req, res) => {
+  try {
+      // Using Sequelize's 'findAll' method to retrieve all posts from the database
+      const listOfUsers = await User.findAll();
+      // Sending the list of posts as a JSON response
+      res.json(listOfUsers);
+  } catch (error) {
+      console.error('Error fetching posts:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// (validation) validateToken is a middleware that checks if the user is logged in then return posts
+router.get("/comment", async (req, res) => {
+  try {
+      // Using Sequelize's 'findAll' method to retrieve all posts from the database
+      const listOfComments = await Comment.findAll({
+          include: {
+              model: User,
+              attributes: ['username','id'], // Include only the 'username' attribute of the User model
+          },
+      });
+      // Sending the list of posts as a JSON response
+      res.json(listOfComments);
+  } catch (error) {
+      console.error('Error fetching posts:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+router.delete("/post/:postid", async (req, res) => {
+  try {
+      const post = await Post.findOne({where: {id: req.params.postid}});
+      if (!post) {
+        return res.json({ error: 'Post not found' });
+      } else {
+        await post.destroy();
+        res.json({ message: 'Post deleted successfully' });
+      }
+  } catch (error) {
+      console.error('Error fetching posts:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+router.delete("/comment/:commentid", async (req, res) => {
+  try {
+      const comment = await Comment.findOne({where: {id: req.params.commentid}});
+      if (!comment) {
+        return res.json({ error: 'Comment not found' });
+      } else {
+        await comment.destroy();
+        res.json({ message: 'Comment deleted successfully' });
+      }
+  } catch (error) {
+      console.error('Error fetching user:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 module.exports = router;

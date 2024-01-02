465e4003d1cc3435e9090c8e6ccd29ca277dea29
SELİM ARSLAN <akif.selim.aa@gmail.com>
2024-01-02 21:02:42 +0300

    header profileimage hatası çözüldü

--- client/src/pages/Header.js
+++ client/src/pages/Header.js
@@ -1,9 +1,10 @@
-import React, { useState, useContext,useEffect } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { UserContext } from '../Helpers/UserContext.js';
 import axios from 'axios';
 import logoImage from '../images/cre8share-logo.png';
 import '../css/Header.css';
+import profileImage from '../images/pp.png';
 
 const Header = () => {
   const [searchType, setSearchType] = useState('username');
@@ -65,7 +66,7 @@ const Header = () => {
     setShowProfileMenu(false);
   };
 
-  
+
 
   const handleInputChange = (e) => {
     setSearchInput(e.target.value);
@@ -82,7 +83,7 @@ const Header = () => {
       console.error('Error searching users:', error);
     }
   };
-  
+
   const arrayBufferToBase64 = (buffer) => {
     const binary = new Uint8Array(buffer.data).reduce(
       (binaryString, byte) => binaryString + String.fromCharCode(byte),
@@ -93,65 +94,70 @@ const Header = () => {
 
   return (
     <div className="header">
-        <div className="header-left">
-          <a href="/home">
-            <img src={logoImage} alt="Logo" />
-          </a>
-        </div>
-        <div className="header-middle">
-          <input
-            type="text"
-            placeholder={`Search by ${searchType === 'username' ? 'Username' : 'Post'}`}
-            value={searchInput}
-            onChange={handleInputChange} // Triggered on every input change
-            style={{ borderRadius: '15%' }}
-          />
-          <button
-            style={{
-              marginLeft: '5px',
-              borderRadius: '25%',
-              backgroundColor: '#4b4242',
-              color: '#000000',
-            }}
-            onClick={handleViewAllUsers}
-          >
-            Search
-          </button>
-          {userSearchResults && userSearchResults.length > 0 && (
-            <div className="user-search-results">
-              <h2>User Search Results</h2>
-              <div className="search-results-list">
-                {userSearchResults.slice(0, 5).map((user, index) => (
-                  <div className="search-result" key={index} onClick={() => navigateToProfile(user.username)}>
-                    {user.username}
-                  </div>
-                ))}
-              </div>
-              <button onClick={handleViewAllUsers}>View All Users</button>
-            </div>
-          )}
-        </div>
-        <button className="create-post-button" style={{borderRadius: '25%',}} onClick={navigateCreatePost}>
-          Create a Post
-        </button>
-        <button className="create-post-button" style={{borderRadius: '25%',}} onClick={navigateImageGeneration}>
-          Cre8 and Share
-        </button>
-        <div className="header-right">
-        {userProfile && userProfile && (
-  <div className="profile-picture" onClick={toggleProfileMenu}>
-    <img src={`data:image/png;base64,${arrayBufferToBase64(Userpfp)}`} alt="Profile" />
-  </div>
-)}
-          {showProfileMenu && (
-            <div className="profile-menu">
-              <button onClick={() => handleProfileMenuClick('profile')}>My Profile</button>
-              <button onClick={() => handleProfileMenuClick('settings')}>Settings</button>
-              <button onClick={() => handleProfileMenuClick('logout')}>Log Out</button>
-            </div>
-          )}
-        </div>
+      <div className="header-left">
+        <a href="/home">
+          <img src={logoImage} alt="Logo" />
+        </a>
       </div>
+      <div className="header-middle">
+        <input
+          type="text"
+          placeholder={`Search by ${searchType === 'username' ? 'Username' : 'Post'}`}
+          value={searchInput}
+          onChange={handleInputChange} // Triggered on every input change
+          style={{ borderRadius: '15%' }}
+        />
+        <button
+          style={{
+            marginLeft: '5px',
+            borderRadius: '25%',
+            backgroundColor: '#4b4242',
+            color: '#000000',
+          }}
+          onClick={handleViewAllUsers}
+        >
+          Search
+        </button>
+        {userSearchResults && userSearchResults.length > 0 && (
+          <div className="user-search-results">
+            <h2>User Search Results</h2>
+            <div className="search-results-list">
+              {userSearchResults.slice(0, 5).map((user, index) => (
+                <div className="search-result" key={index} onClick={() => navigateToProfile(user.username)}>
+                  {user.username}
+                </div>
+              ))}
+            </div>
+            <button onClick={handleViewAllUsers}>View All Users</button>
+          </div>
+        )}
+      </div>
+      <button className="create-post-button" style={{ borderRadius: '25%', }} onClick={navigateCreatePost}>
+        Create a Post
+      </button>
+      <button className="create-post-button" style={{ borderRadius: '25%', }} onClick={navigateImageGeneration}>
+        Cre8 and Share
+      </button>
+      
+        <div className="header-right">
+  {userProfile && userProfile && (
+    <div className="profile-picture" onClick={toggleProfileMenu}>
+      {Userpfp ? (
+        <img src={`data:image/png;base64,${arrayBufferToBase64(Userpfp)}`} alt="Profile" />
+      ) : (
+        <img src={profileImage} alt="Default Profile" />
+      )}
+    </div>
+  )}
+        {showProfileMenu && (
+          <div className="profile-menu">
+            <button onClick={() => handleProfileMenuClick('profile')}>My Profile</button>
+            <button onClick={() => handleProfileMenuClick('settings')}>Settings</button>
+            <button onClick={() => handleProfileMenuClick('logout')}>Log Out</button>
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
 
